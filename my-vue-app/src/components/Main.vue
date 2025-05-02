d918f2ac56e6b1787e12db97514d454941a6c56a
Paul <paulnyamawi18@gmail.com>
2025-05-02 23:06:42 +0300

    sleeping time

--- my-vue-app/src/components/Main.vue
+++ my-vue-app/src/components/Main.vue
@@ -1,13 +1,25 @@
 <template>
   <main class="mt-12 justify-center mx-2 text-center">
     <div class="grid grid-cols-2 items-center justify-center gap-10 mt-8">
-        <div class="bg-[#e9f3ff] w-full h-72 rounded-lg">
-            <div class="mt-16 text-left pl-10 ">
-                <h2 class="text-3xl font-semibold">Are you looking <br> for a car?</h2>
-            </div>
+      <div class="bg-[#e9f3ff] w-full h-72 rounded-lg">
+        <div class="mt-16 text-left pl-10">
+          <h2 class="text-3xl font-semibold">
+            Are you looking <br />
+            for a car?
+          </h2>
+          <p class="text-gray-500 text-md mt-4">
+            We are commited to providing our customers <br />
+            with professional services
+          </p>
+
+          <Button
+            class="border-2 border-purple-600 rounded-lg px-3 py-2 text-purple-400 cursor-pointer hover:bg-purple-600 hover:text-purple-200 mt-6"
+          >
+            GET STARTED
+          </Button>
         </div>
-        <div class="bg-[#ffeaf3] opacity-45 w-full h-72 rounded-lg">hello</div>
-        
+      </div>
+      <div class="bg-[#ffeaf3] opacity-45 w-full h-72 rounded-lg">hello</div>
     </div>
   </main>
 </template>
