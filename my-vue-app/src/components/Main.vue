618951b702763ec0b7d1b94b6244b68d72a59fa5
Paul <paulnyamawi18@gmail.com>
2025-05-04 23:08:03 +0300

    adding section

--- my-vue-app/src/components/Main.vue
+++ my-vue-app/src/components/Main.vue
@@ -1,6 +1,9 @@
 <template>
   <main class="mt-12 justify-center mx-2 text-center">
-    <div class="grid grid-cols-2 items-center justify-center gap-10 mt-8">
+    <div
+      id="section-main"
+      class="grid grid-cols-2 items-center justify-center gap-10 mt-8"
+    >
       <div class="bg-[#e9f3ff] w-full h-72 rounded-lg">
         <div class="mt-16 text-left pl-10">
           <h2 class="text-3xl font-semibold">
@@ -18,7 +21,10 @@
             >
               GET STARTED
             </Button>
-            <font-awesome-icon :icon="['fas', 'car']" class="text-6xl pr-10 text-purple-400"/>
+            <font-awesome-icon
+              :icon="['fas', 'car']"
+              class="text-6xl pr-10 text-purple-400"
+            />
           </div>
         </div>
       </div>
@@ -39,14 +45,23 @@
             >
               GET STARTED
             </Button>
-            <font-awesome-icon :icon="['fas', 'car']" class="text-6xl pr-10 text-purple-400"/>
+            <font-awesome-icon
+              :icon="['fas', 'car']"
+              class="text-6xl pr-10 text-purple-400"
+            />
           </div>
         </div>
       </div>
     </div>
+
+    <!-- popular car -->
+    <mostCar />
+    <!--  -->
   </main>
 </template>
 
-<script setup lang="ts"></script>
+<script setup lang="ts">
+import mostCar from "./mostCar.vue";
+</script>
 
 <style scoped></style>
--- my-vue-app/src/components/Photo.vue
+++ my-vue-app/src/components/Photo.vue
@@ -7,7 +7,7 @@
     <div class="justify-center text-center mt-4">
       <h1 class="text-2xl font-bold text-[#181d30]">Browse by Type</h1>
 
-      <div class="grid grid-cols-7 pt-8 mx-4 gap-4">
+      <div id="type-car" class="grid grid-cols-7 pt-8 mx-4 gap-4">
         <div
           class="bg-white p-8 rounded-md shadow-md flex flex-col items-center justify-center cursor-pointer hover:shadow-lg"
         >
