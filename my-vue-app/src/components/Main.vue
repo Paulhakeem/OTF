4ace5110d00730ab0491982d5fbd3b06675ef27a
Paul <paulnyamawi18@gmail.com>
2025-05-04 22:01:20 +0300

    adding section

--- my-vue-app/src/components/Main.vue
+++ my-vue-app/src/components/Main.vue
@@ -12,14 +12,37 @@
             with professional services
           </p>
 
-          <Button
-            class="border-2 border-purple-600 rounded-lg px-3 py-2 text-purple-400 cursor-pointer hover:bg-purple-600 hover:text-purple-200 mt-6"
-          >
-            GET STARTED
-          </Button>
+          <div class="flex items-center justify-between mt-6">
+            <Button
+              class="border-2 border-purple-600 rounded-lg px-3 py-2 text-purple-400 cursor-pointer hover:bg-purple-600 hover:text-purple-200 mt-6"
+            >
+              GET STARTED
+            </Button>
+            <font-awesome-icon :icon="['fas', 'car']" class="text-6xl pr-10 text-purple-400"/>
+          </div>
         </div>
       </div>
-      <div class="bg-[#ffeaf3] opacity-45 w-full h-72 rounded-lg">hello</div>
+      <div class="bg-[#ffeaf3] w-full h-72 rounded-lg">
+        <div class="mt-16 text-left pl-10">
+          <h2 class="text-3xl font-semibold">
+            Do you want <br />
+            to sale car?
+          </h2>
+          <p class="text-gray-500 text-md mt-4">
+            We are commited to providing our customers <br />
+            with professional services
+          </p>
+
+          <div class="flex items-center justify-between mt-6">
+            <Button
+              class="border-2 border-purple-600 rounded-lg px-3 py-2 text-purple-400 cursor-pointer hover:bg-purple-600 hover:text-purple-200 mt-6"
+            >
+              GET STARTED
+            </Button>
+            <font-awesome-icon :icon="['fas', 'car']" class="text-6xl pr-10 text-purple-400"/>
+          </div>
+        </div>
+      </div>
     </div>
   </main>
 </template>
